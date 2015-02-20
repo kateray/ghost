39a78968b6e8393e1fde9ccf2e9f98530fb34e11
Kate Ray <email@kateray.net>
2015-02-19 23:06:32 -0500

    moar glitch

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,17 @@
 $( document ).ready(function() {
 	window.setInterval(function(){
+		$('#sentence').addClass('glitch2');
 		$.ajax({
 			url: '/n',
 			success: function(resp){
 				var text = '$ ' + resp;
-				$('#sentence').attr('data-text', text).text(text);
+				window.setTimeout(function(){
+					$('#sentence').attr('data-text', text).text(text);
+					$('#sentence').removeClass('glitch2');
+				}, 1000);
 			}
 		});
-	}, 4000 );
+	}, 4500 );
 	/*window.addEventListener('keydown', function(e){
 		location.reload();
 	});*/
